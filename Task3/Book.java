d03c6b8cfaade97ecb6a8b45f0b2259cd57f9d63
Shivam <shivamfeb77@gmail.com>
2025-08-08 17:11:23 +0530

    added book class with it methods

---
+++ Task3/Book.java
@@ -0,0 +1,54 @@
+package Task3;
+public class Book {
+
+    // Instance variables 
+
+    private String bookName;  
+    private String author;
+    private boolean isBorrowed;
+
+    public Book(String bookName, String author) {
+        this.bookName = bookName;
+        this.author = author;
+        this.isBorrowed = false;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    // method which tell book is borrowed or not
+    public boolean isBorrow(){
+
+        return isBorrowed;
+    }
+
+    // method which make isBorrowed true is someone issued a book 
+    public void borrowedBook(Book book){
+        isBorrowed = true;
+    }
+
+    // method which make isBorrowed false if book is returned
+    public void returnBook(Book book){
+        isBorrowed = false;
+    }
+
+    @Override
+    public String toString() {
+        return "Book [bookName=" + bookName + ", author=" + author + ", isBorrowed=" + isBorrowed + "]";
+    }
+
+
+
+
+
+    
+
+    
+
+    
+}
